886cd5a91f813241607c7f1245ec1538a489586a
Sabrina Hanson <sabhanson7@gmail.com>
2021-12-23 17:38:15 -0800

    initial prompt asking password length is working correctly. it will loop until the user enters a valid number between 8 and 128.

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","]","}","|","/",":",";","<",",",">",".","?"]
-var numbers = ["0","1","2","3","4","5","6","7","8","9"]
+//these constants contain all of the possible variables.
+const lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+const upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","]","}","|","\\","/",":",";","<",",",">",".","?"]
+const numbers = ["0","1","2","3","4","5","6","7","8","9"]
 
-
+//function to generate password with all user inputs: length, lower/uppercase letters, symbols, and/or numbers
 function generatePassword() {
-  window.confirm("Would you like me to create a password for you?");
+  //password output is an empty string that will be filled at the end of this function
+  var passwordOutput = "";
+
+  //randomArray is an empty array that will be filled by input from four if statements asking for which constants to use
+  var randomArray = [];
+
+  //passLength will generate a window prompt that the user must give a value to
   var passLength = window.prompt("How many characters would you like your password to be? You must choose a number between 8 and 128");
-  alert ("You said " + passLength + " characters.");
-  var lowerConfirm = confirm("Would you like your password to contain lowercase letters?") 
-  var upperConfirm = confirm("Would you like your password to contain uppercase letters?") 
-  var symbolsConfirm = confirm("Would you like your password to contain symbols?") 
-  var numberssConfirm = confirm("Would you like your password to contain numbers?") 
+
+  //passwordLength must meet certain requirements. while it fails to meet these requirements, the user will be prompted to try again.
+  while (isNaN(passLength) || passLength < 8 || passLength > 128) {
+    var passLength = window.prompt("Not a valid input. Try again");
+  }
+  //once user inputs a valid number, they will be alerted with this string
+  window.alert ("You said you'd like your password to be " + passLength + " characters. Thank you.")
+
+  while (randomArray.length === 0) {
+    if(window.confirm("Would you like your password to include lowercase letters?")) {
+      randomArray = randomArray.concat(lower)
+    }
+  }
+
 }
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
